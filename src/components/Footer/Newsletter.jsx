942c370ac49d6193073cbbb17710c11871586e0c
Joy Sarkar <sjoy.cse3.bu@gmail.com>
2022-07-16 00:43:25 +0600

    find us and newsletter part complete

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 
+import  SubHeading from '../SubHeading/SubHeading'
+
 import './Newsletter.css';
 
 const Newsletter = () => (
-  <div>
-    Newsletter
+  <div className='app__newsletter'>
+    <div className='app__newsletter-heading'>
+      <SubHeading title="NewsLetter"/>
+      <h1 className='headtext__cormorant'>Subscribe To Our Newsletter</h1>
+      <p className='p__opensans'>Maxime mollitia, molestiae quas vel sint</p>
+      <div className='app__newsletter-input flex__center'>
+        <input type="email" placeholder="enter your email"/>
+        <button className='custom__button'>Subscribe</button>
+      </div>
+    </div>
+    
   </div>
 );
 
